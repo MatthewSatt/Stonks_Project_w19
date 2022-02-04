d72c462c51d0ffdd3b9440ce80ecefc1db855da3
Andres Soca <lumen.development01@gmail.com>
2022-02-04 18:21:11 -0500

    css updates on navbar

--- python-project-starter/react-app/src/components/Navigation/index.js
+++ python-project-starter/react-app/src/components/Navigation/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { FaAngleDown, FaAngleUp, FaChartLine } from 'react-icons/fa';
-// import { NavLink, Link } from 'react-dom';
-// import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useState } from "react"
 // import Demo from './demo-user';
 
@@ -12,37 +10,18 @@ function Navbar({ isLoaded }){
   const [navbarOpen, setNavbarOpen] = useState(false)
   const [navbarOpen2, setNavbarOpen2] = useState(false)
   const [navbarOpen3, setNavbarOpen3] = useState(false)
-//   const sessionUser = useSelector(state => state.session.user);
 
-//   let sessionLinks;
-//   if (sessionUser) {
-//     sessionLinks = (
-//       <>
-//         <NavLink className="become__host" to={'/host'}>Portfolio</NavLink>
-//         <ProfileButton user={sessionUser} />
-//       </>
-//     );
-//   } else {
-//     sessionLinks = (
-//       <>
-//         <NavLink className="login" to="/login">Log In</NavLink>
-//         <NavLink className="signup" to="/signup">Sign Up</NavLink>
-//         <Demo />
-//       </>
-//     );
-//   }
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  }
 
-const handleToggle = () => {
-  setNavbarOpen(prev => !prev)
-}
+  const handleToggle2 = () => {
+    setNavbarOpen2(prev => !prev)
+  }
 
-const handleToggle2 = () => {
-  setNavbarOpen2(prev => !prev)
-}
-
-const handleToggle3 = () => {
-  setNavbarOpen3(prev => !prev)
-}
+  const handleToggle3 = () => {
+    setNavbarOpen3(prev => !prev)
+  }
 
   return (
 
@@ -52,13 +31,16 @@ const handleToggle3 = () => {
       </div>
       <div className='all__links'>
         <span>
-          <NavLink onClick={handleToggle} className='products__tab' to='/'>Products {navbarOpen ? < FaAngleDown/> : < FaAngleUp/>}</NavLink>
+          <NavLink onClick={handleToggle} className='products__tab' to='/'>Products {navbarOpen ? < FaAngleUp/> : < FaAngleDown/>}</NavLink>
+          <ul className={`menuNav ${navbarOpen ? "showMenu" : ""}`}>...</ul>
         </span>
         <span>
-          <NavLink onClick={handleToggle2} className='learn__tab' to='/'>Learn {navbarOpen2 ? < FaAngleDown/> : < FaAngleUp/>}</NavLink>
+          <NavLink onClick={handleToggle2} className='learn__tab' to='/'>Learn {navbarOpen2 ? < FaAngleUp/> : < FaAngleDown/>}</NavLink>
+          <ul className={`menuNav ${navbarOpen2 ? "showMenu" : ""}`}>...</ul>
         </span>
         <span>
-          <NavLink onClick={handleToggle3} className='whoweare__tab' to='/'>Who we are {navbarOpen3 ? < FaAngleDown/> : < FaAngleUp/>}</NavLink>
+          <NavLink onClick={handleToggle3} className='whoweare__tab' to='/'>Who we are {navbarOpen3 ? < FaAngleUp/> : < FaAngleDown/>}</NavLink>
+          <ul className={`menuNav ${navbarOpen3 ? "showMenu" : ""}`}>...</ul>
         </span>
       </div>
       <nav>
