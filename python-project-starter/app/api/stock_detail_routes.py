4bc5f4f52c4c437a6ace25db2547615eea75bdbb
will <w.duffy@outlook.com>
2022-02-05 17:34:51 -0600

    updated the stock detail store routes and api

--- python-project-starter/app/api/stock_detail_routes.py
+++ python-project-starter/app/api/stock_detail_routes.py
@@ -1,13 +1,40 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
 import json, requests
+import os
 
 stock_detail_routes = Blueprint("stonk", __name__)
 
-@stock_detail_routes.route('/stonk/<ticker>')
+@stock_detail_routes.route('/<ticker>')
+def get_company_stats(ticker):
+    company_stats = {}
 
-def stockdetails(ticker):
-    company_stats = {ticker: {}}
-    ticker = ticker.upper()
+    url = "https://yh-finance.p.rapidapi.com/stock/v2/get-profile"
 
-    def get_data(ticker):
-        company_stats = {}
+    querystring = {"symbol": ticker.upper(),"region":"US"}
+
+    headers = {
+        'x-rapidapi-host': "yh-finance.p.rapidapi.com",
+        'x-rapidapi-key': os.environ.get("RAPID_API_KEY")
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    object = response.json()
+
+    company_stats["price"] = object["price"]["regularMarketPrice"]["raw"]
+    company_stats["name"] = object["price"]["longName"]
+    company_stats["about"] = object["assetProfile"]["longBusinessSummary"]
+    company_stats["employees"] = object["assetProfile"]["fullTimeEmployees"]
+    company_stats["city"] = object["assetProfile"]["city"]
+    company_stats["state"] = object["assetProfile"]["state"]
+    company_stats["sector"] = object["assetProfile"]["sector"]
+    company_stats["volume"] = object["price"]["regularMarketVolume"]["fmt"]
+    company_stats["avgvolume"] = object["price"]["averageDailyVolume10Day"]["fmt"]
+    company_stats["marketcap"] = object["price"]["marketCap"]["fmt"]
+    company_stats["peratio"] = object["summaryDetail"]["forwardPE"]["fmt"]
+    company_stats["divyield"] = object["summaryDetail"]["dividendYield"]["fmt"]
+    company_stats["52high"] = object["summaryDetail"]["fiftyTwoWeekHigh"]["raw"]
+    company_stats["52low"] = object["summaryDetail"]["fiftyTwoWeekLow"]["raw"]
+
+    print("COMPANY STATS", company_stats)
+    return company_stats
--- python-project-starter/react-app/src/store/stockDetails.js
+++ python-project-starter/react-app/src/store/stockDetails.js
@@ -1,14 +1,35 @@
 
 const SET_STOCK_DETAIL = 'stock/setStockDetail'
 
-const setStockDetail = (details) =>{
+const setStockDetail = (stockDetails) =>{
     return {
         type: SET_STOCK_DETAIL,
-        details
+        stockDetails
     }
 }
 
 
 export const getStockDetails = (ticker) => async (dispatch) => {
-    const res = await fetch(`/api/`)
+    const res = await fetch(`/api/stonk/${ticker}`)
+    if(res.ok) {
+        const stockDetails = await res.json();
+        dispatch(setStockDetail(stockDetails))
+    }
 }
+
+const initialState = {};
+
+const stockDetailReducer = (state = initialState, action) =>{
+    let newState;
+    switch (action.type){
+        case SET_STOCK_DETAIL:
+            newState = { ... state }
+            newState.stockDetail = action.stockDetails
+
+            return newState
+        default:
+            return state
+    }
+};
+
+export default stockDetailReducer
