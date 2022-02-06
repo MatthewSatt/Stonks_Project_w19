6e689e2e200936cb721ba01fc6e699c3e87e57bb
will <w.duffy@outlook.com>
2022-02-05 20:16:49 -0600

    installed pandas.  Created the api to pull from external API and return dates and values for stonk/ticker

--- python-project-starter/app/api/stock_detail_routes.py
+++ python-project-starter/app/api/stock_detail_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 import json, requests
 import os
+import pandas as pd
 
 stock_detail_routes = Blueprint("stonk", __name__)
 
@@ -37,5 +38,37 @@ def get_company_stats(ticker):
     company_stats["divyield"] = object["summaryDetail"]["dividendYield"]["fmt"]
     company_stats["52high"] = object["summaryDetail"]["fiftyTwoWeekHigh"]["raw"]
     company_stats["52low"] = object["summaryDetail"]["fiftyTwoWeekLow"]["raw"]
+
+    #Code below is for historical prices.  "Outputsize": "compact" returns 100 results "full" returns 20 years
+
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+
+    querystring = {"function":"TIME_SERIES_DAILY","symbol": ticker.upper(),"outputsize":"compact","datatype":"json"}
+
+    headers = {
+    'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+    'x-rapidapi-key': os.environ.get("RAPID_API_KEY")
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    object = response.json()
+
+    values_to_destructure = object["Time Series (Daily)"]
+
+    #Creates an array of all the dates
+    dates = []
+    for key in values_to_destructure:
+        dates.append(key)
+
+    #creates an array of all the historical prices
+    values = []
+    panda_data = pd.DataFrame(values_to_destructure)
+    panda_data_t = panda_data.T
+    for index, row in panda_data_t.iterrows():
+        values.append(row["4. close"])
+
+    company_stats["dates"] = dates
+    company_stats["values"] = values
 
     return(company_stats)
