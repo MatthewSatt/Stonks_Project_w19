1d9cdabf0a07f686e9310287ad1b218bbc0d5baf
will <w.duffy@outlook.com>
2022-02-08 15:24:28 -0600

    removed numpy and updated search bar css and logic

--- python-project-starter/react-app/src/components/SearchBar/index.js
+++ python-project-starter/react-app/src/components/SearchBar/index.js
@@ -4,10 +4,13 @@ import "./searchBar.css"
 
 function SearchBar() {
 
-    const [searchTerm, setSearchTerm] = useState();
+    const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([]);
 
     useEffect(async (e) =>{
+        if (searchTerm === ""){
+            return setSearchTerm("")
+        }
         const res = await fetch(`/api/search`, {
 	"method": "GET",
 	"headers": {
@@ -15,29 +18,35 @@ function SearchBar() {
 	}
 })
     const result = await res.json()
-    console.log("RESULT IN COMPONENT", result)
+
+
     const filteredResult = result.filter(word =>{
         return word[0].includes(searchTerm)
     })
     const filteredData = result[0][1]
     const finalResult = filteredResult.slice(0, 5)
     setSearchResults(finalResult)
-        // )=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
     }, [searchTerm])
+
+
 
     console.log("FINAL RESULT", searchResults)
     return (
         <div className='search_container'>
         <div className="search__bar">
-            <input type="text" placeholder="Search Stonks.." onChange={(e)=>setSearchTerm(e.target.value.toUpperCase())}></input>
+            <input type="text" value={searchTerm} placeholder="Search Stonks.." onChange={(e)=>setSearchTerm(e.target.value.toUpperCase())}></input>
 
         </div>
         <div id="search_results">
-            {searchResults.map((result) => (
+            {searchTerm && (
                 <>
-                <Link className="test" to={`/stonk/${result[0]}`}> {result} </Link>
+                {searchResults.map((result) => (
+                    <>
+                    <Link className="test" to={`/stonk/${result[0]}`}> {result} </Link>
+                    </>
+                    ))}
                 </>
-            ))}
+            )}
         </div>
         </div>
     )
