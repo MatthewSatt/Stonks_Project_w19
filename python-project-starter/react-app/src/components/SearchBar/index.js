ba66ef4955846864a4da2b6a11ae6410b0d5e083
will <w.duffy@outlook.com>
2022-02-08 12:15:18 -0600

    almost finised search need to style

--- python-project-starter/react-app/src/components/SearchBar/index.js
+++ python-project-starter/react-app/src/components/SearchBar/index.js
@@ -1,20 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import {Link} from 'react-router-dom'
 import "./searchBar.css"
 
 function SearchBar() {
 
     const [searchTerm, setSearchTerm] = useState();
+    const [searchResults, setSearchResults] = useState([]);
 
-    useEffect(() =>{
-        // const filteredData = cryptoLsit?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        console.log(searchTerm)
+    useEffect(async (e) =>{
+        const res = await fetch(`/api/search`, {
+	"method": "GET",
+	"headers": {
+		"Content-Type": "application/json"
+	}
+})
+    const result = await res.json()
+    console.log("RESULT IN COMPONENT", result)
+    const filteredResult = result.filter(word =>{
+        return word[0].includes(searchTerm)
+    })
+    const filteredData = result[0][1]
+    const finalResult = filteredResult.slice(0, 5)
+    setSearchResults(finalResult)
+        // )=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
     }, [searchTerm])
 
+    console.log("FINAL RESULT", searchResults)
     return (
+        <>
         <div className="search__bar">
             <input type="text" placeholder="Search Stonks.." onChange={(e)=>setSearchTerm(e.target.value)}></input>
-            
+
         </div>
+        <div>
+            {searchResults.map((result) => (
+                <Link to={`/stonk/${result[0]}`}> {result} </Link>
+            ))}
+        </div>
+        </>
     )
 }
 
