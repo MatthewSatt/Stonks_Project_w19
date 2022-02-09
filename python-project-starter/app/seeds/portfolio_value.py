7d4806fa05bff64aaf96060e133369aa991ea35c
will <w.duffy@outlook.com>
2022-02-08 21:46:36 -0600

    created logic in the user model to add a new portfolio value..may reconfigure to use APScheduler

--- python-project-starter/app/seeds/portfolio_value.py
+++ python-project-starter/app/seeds/portfolio_value.py
@@ -1,16 +1,21 @@
 from app.models import db, PortfolioValue
+from datetime import datetime, timedelta
 
 
 def seed_portfolio_value():
+    today = datetime.now()
+    day_before = today - timedelta(days = 1)
+    day_before2 = today - timedelta(days = 2)
+    day_before3 = today - timedelta(days = 3)
 
     portfolio_val1 = PortfolioValue(
-       value=6000.60, date="2/4/2022",  user_id=1)
+       value=6000.60, date=day_before,  user_id=1)
 
     portfolio_val2 = PortfolioValue(
-        value=7000.50, date="2/3/2022", user_id=1)
+        value=7000.50, date=day_before2, user_id=1)
 
     portfolio_val3 = PortfolioValue(
-        value=6400, date="2/2/2022", user_id=1)
+        value=6400, date=day_before3, user_id=1)
 
     db.session.add(portfolio_val1)
     db.session.add(portfolio_val2)
